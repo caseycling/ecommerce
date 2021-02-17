ed5f830f8c1fbf74b8f03f1c2986cbf14cc78491
Casey <caseyclinga@gmail.com>
2021-02-17 15:24:31 -0800

    Added styles for product layout

--- src/components/Products/Product/Product.jsx
+++ src/components/Products/Product/Product.jsx
@@ -7,11 +7,12 @@ import {
   Typography,
   IconButton,
 } from '@material-ui/core';
-
 import { AddShoppingCart } from '@material-ui/icons';
-import classes from '*.module.css';
+
+import useStyles from './styles';
 
 const Product = (props) => {
+  const classes = useStyles();
   return (
     <Card className={classes.root}>
       <CardMedia className={classes.media} image='' title={product.name} />
