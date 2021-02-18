84cff54961e380db6091c2119c6c5669a37520d2
Casey <caseyclinga@gmail.com>
2021-02-17 18:33:22 -0800

    Added fetch calls to dynamically render products from commerce.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,41 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-// import Products from './components/Products/Products';
-// import NavBar from './components/NavBar/NavBar';
-
+import { commerce } from './lib/commerce';
 import { Products, Navbar } from './components';
 
-function App() {
+const App = () => {
+  const [products, setProducts] = useState([]);
+  const [cart, setCart] = useState({});
+
+  //Fetch products
+  const fetchProducts = async () => {
+    const { data } = await commerce.products.list();
+
+    setProducts(data);
+  };
+
+  //Fetch cart and set to response
+  const fetchCart = async () => {
+    setCart(await commerce.cart.retrieve());
+  };
+
+  const handleAddToCart = async (productId, quantity) => {
+    const item = await commerce.cart.add(productId, quantity);
+
+    setCart(item.cart);
+  };
+
+  useEffect(() => {
+    fetchProducts();
+    fetchCart();
+  }, []);
+
   return (
     <div>
       <Navbar />
-      <Products />
+      <Products products={products} onAddToCart={handleAddToCart} />
     </div>
   );
-}
+};
 
 export default App;
