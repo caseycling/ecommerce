f60d08872fed9c9279b0fffa07e314adf4689619
Casey <caseyclinga@gmail.com>
2021-02-17 18:32:37 -0800

    Added fetch calls to dynamically render products from commerce.js

--- src/components/Products/Product/Product.jsx
+++ src/components/Products/Product/Product.jsx
@@ -11,14 +11,16 @@ import { AddShoppingCart } from '@material-ui/icons';
 
 import useStyles from './styles';
 
-const Product = ({ product }) => {
+const Product = ({ product, onAddToCart }) => {
   const classes = useStyles();
+
+  console.log(product);
 
   return (
     <Card className={classes.root}>
       <CardMedia
         className={classes.media}
-        image={product.image}
+        image={product.media.source}
         title={product.name}
       />
       <CardContent>
@@ -26,14 +28,21 @@ const Product = ({ product }) => {
           <Typography variant='h5' gutterBottom>
             {product.name}
           </Typography>
-          <Typography variant='h5'>{product.price}</Typography>
+          <Typography variant='h5'>
+            {product.price.formatted_with_symbol}
+          </Typography>
         </div>
-        <Typography variant='body2' color='textSecondary'>
-          {product.description}
-        </Typography>
+        <Typography
+          dangerouslySetInnerHTML={{ __html: product.description }}
+          variant='body2'
+          color='textSecondary'
+        />
       </CardContent>
       <CardActions disableSpacing className={classes.cardActions}>
-        <IconButton aria-label='Add to cart'>
+        <IconButton
+          aria-label='Add to cart'
+          onClick={() => onAddToCart(product.id)}
+        >
           <AddShoppingCart />
         </IconButton>
       </CardActions>
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -4,35 +4,15 @@ import { Grid } from '@material-ui/core';
 import Product from './Product/Product';
 import useStyles from './styles';
 
-const products = [
-  {
-    id: 1,
-    name: 'Shoes',
-    description: 'Running Shoes',
-    price: '$60',
-    image:
-      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT5I-fNzSQxpKln2uxbT0RjQ9119dgG3pVHbtAUhsA1rTXwEtIOiygl7FzfdMj0mQLMKtjbXDI&usqp=CAc',
-  },
-  {
-    id: 2,
-    name: 'Macbook',
-    description: 'Apple macbook',
-    price: '$300',
-    image:
-      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQrDekrm1B5G55783QJJAtW7WpO98263juZTfDROf85d0vDvjGtp0oRpYGcSG0hgUbufRxOOY&usqp=CAc',
-  },
-];
-
-const Products = () => {
+const Products = ({ products, onAddToCart }) => {
   const classes = useStyles();
-
   return (
     <main className={classes.content}>
       <div className={classes.toolbar} />
       <Grid container justify='center' spacing={4}>
         {products.map((product) => (
           <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
-            <Product product={product} />
+            <Product product={product} onAddToCart={onAddToCart} />
           </Grid>
         ))}
       </Grid>
