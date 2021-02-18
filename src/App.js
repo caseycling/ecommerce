12e58dd75d2634a1060c3781d0aea09e1c28504f
Casey <caseyclinga@gmail.com>
2021-02-17 16:08:18 -0800

    Created and imported basic navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
-import Products from './components/Products/Products';
+
+// import Products from './components/Products/Products';
+// import NavBar from './components/NavBar/NavBar';
+
+import { Products, Navbar } from './components';
 
 function App() {
   return (
     <div>
+      <Navbar />
       <Products />
     </div>
   );
