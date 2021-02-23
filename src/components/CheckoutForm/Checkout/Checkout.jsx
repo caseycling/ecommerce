db8b5ef984bda7d493b3ba69074577b6706f76b5
Casey <caseyclinga@gmail.com>
2021-02-23 15:11:20 -0800

    Created logic to fetch countries for shipping form

--- src/components/CheckoutForm/Checkout/Checkout.jsx
+++ src/components/CheckoutForm/Checkout/Checkout.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   Paper,
   Stepper,
@@ -9,6 +9,8 @@ import {
   Divider,
   Button,
 } from '@material-ui/core';
+
+import { commerce } from '../../../lib/commerce';
 import AddressForm from './AddressForm';
 import PaymentForm from './PaymentForm';
 
@@ -16,13 +18,36 @@ import useStyles from './styles';
 
 const steps = ['Shipping address', 'Payment details'];
 
-const Checkout = () => {
-  const [activeStep, setActiveStep] = useState(2);
+const Checkout = ({ cart }) => {
+  const [activeStep, setActiveStep] = useState(0);
+  const [checkoutToken, setCheckoutToken] = useState(null);
   const classes = useStyles();
+
+  useEffect(() => {
+    const generateToken = async () => {
+      try {
+        const token = await commerce.checkout.generateToken(cart.id, {
+          type: 'cart',
+        });
+
+        console.log(token);
+
+        setCheckoutToken(token);
+      } catch (err) {}
+    };
+
+    generateToken();
+  }, [cart]);
 
   const Confirmation = () => <div>Confirmation</div>;
 
-  const Form = () => (activeStep === 0 ? <AddressForm /> : <PaymentForm />);
+  const Form = () =>
+    activeStep === 0 ? (
+      <AddressForm checkoutToken={checkoutToken} />
+    ) : (
+      <PaymentForm />
+    );
+
   return (
     <>
       <div className={classes.toolbar}>
@@ -38,7 +63,11 @@ const Checkout = () => {
                 </Step>
               ))}
             </Stepper>
-            {activeStep === steps.length ? <Confirmation /> : <Form />}
+            {activeStep === steps.length ? (
+              <Confirmation />
+            ) : (
+              checkoutToken && <Form />
+            )}
           </Paper>
         </main>
       </div>
